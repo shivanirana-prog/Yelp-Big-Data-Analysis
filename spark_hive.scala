a6345249851d668c6e62125485642d004eab0bd2
mohitroy2502 <54563792+mohitroy2502@users.noreply.github.com>
2019-11-06 13:43:01 -0600

    Create spark_hive.scala

---
+++ spark_hive.scala
@@ -0,0 +1,30 @@
+// read the tip json data
+// transform it
+// store it to hdfs 
+val tipDF = sqlContext.jsonFile("/user/cloudera/rawdata/yelp/tips")
+val refinedTipDF = tipDF.select("user_id","business_id","likes","text","date").repartition(1)
+
+refinedTipDF.write.parquet("/user/cloudera/output/yelp/tips")
+
+//we build the hive tip table on top of this output to allow users query our data
+
+
+import org.apache.spark.sql.Row
+
+
+//integrating spark and hive using the hive context
+import org.apache.spark.sql.hive.HiveContext
+
+val hiveCtx = new HiveContext(sc)
+import hiveCtx.implicits._
+
+
+
+
+
+
+//Another wai write directly to a hive table already created in hive using insertInto command
+
+val reviewDF = hiveCtx.jsonFile("/user/cloudera/rawdata/yelp/reviews").
+		select("review_id","user_id","business_id","stars","text","date","votes.cool","votes.funny","votes.useful")
+reviewDF.write.insertInto("yelp.review")
